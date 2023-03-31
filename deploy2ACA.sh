7c41d4c5a785ae0215bb18d78ce5edd0becba258
Arturo-Quiroga-MSFT <32229456+Arturo-Quiroga-MSFT@users.noreply.github.com>
2023-03-31 13:04:14 -0400

    hola

--- deploy2ACA.sh
+++ deploy2ACA.sh
@@ -2,8 +2,8 @@
 
 az containerapp up \
 --name pythonapp \
---resource-group newapp-rg \
---environment newapp-env \
+--resource-group my-rg \
+--environment my-env \
 --location canadacentral \
 --ingress external \
 --source .
@@ -11,6 +11,9 @@ az containerapp up \
 
 
 
+AZURE_CLIENT_ID=
+AZURE_TENANT_ID=
+AZURE_SUBSCRIPTION_ID=
 
 
 
@@ -20,21 +23,3 @@ az containerapp up \
 
 
 
-
-
-
-
-Creating Containerapp pythonapp in resource group my-rg
-Adding registry password as a secret with name "cafcd1ab3c48acrazurecrio-cafcd1ab3c48acr"
-
-Container app created. Access your app at https://pythonapp.redsand-ac8f758c.canadacentral.azurecontainerapps.io/
-
-
-Your container app pythonapp has been created and deployed! Congrats! 
-
-Your app is running image cafcd1ab3c48acr.azurecr.io/pythonapp:20230330170608199219 and listening on port 8080
-Browse to your container app at: http://pythonapp.redsand-ac8f758c.canadacentral.azurecontainerapps.io 
-
-Stream logs for your container with: az containerapp logs show -n pythonapp -g my-rg 
-
-See full output using: az containerapp show -n pythonapp -g my-rg 
